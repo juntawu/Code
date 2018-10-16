986153274c1815c4d25acbd60f910cf2eb784edb
juntawu <juntawu@163.com>
2018-10-16 13:04:42 +0800

    Update

---
+++ LeetCode/006_ZigZag_Conversion/Solution_C++_v1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+		string output="";
+		for(int i=0; i<numRows; i++){
+			output += 
+		}
+		
+		return output;
+    }
+	
+};
+
+
+
+int main(){
+	Solution solver = Solution();
+	string str;
+	
+	return 0;
+}
