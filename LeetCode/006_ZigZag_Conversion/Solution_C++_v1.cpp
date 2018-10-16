1e675ac7d21a1ffc79b1bddc8bb9f6258e010703
juntawu <juntawu@163.com>
2018-10-16 13:33:12 +0800

    update

--- LeetCode/006_ZigZag_Conversion/Solution_C++_v1.cpp
+++ LeetCode/006_ZigZag_Conversion/Solution_C++_v1.cpp
@@ -3,24 +3,40 @@
 using namespace std;
 
 
+
 class Solution {
 public:
     string convert(string s, int numRows) {
-		string output="";
-		for(int i=0; i<numRows; i++){
-			output += 
-		}
+        string result = "";
+		// output in one line
+		if(1 == numRows)
+			return s;
 		
-		return output;
+		// find the rule: split the string into several sub-strings according to the numRows
+        int step = 2 * numRows - 2;  // length of sub-string
+        int len = s.size();  // length of the whole string
+        for(int i=0; i<numRows; i++){       
+            int pt = 0;  // base index of the sub-string
+            while(pt >=0 && pt <len){
+                if(pt + i >= 0 && pt + i < len)
+                    result += s[pt + i];  // first character of sub-string in (i+1)-th line
+                if(i>0 && step-i > i && pt+(step-i) < len)
+                    result += s[pt+(step-i)];  // second character of sub-string in (i+1)-th line
+                pt += step;
+            }
+        }
+		
+		return result;
     }
-	
 };
 
 
 
 int main(){
 	Solution solver = Solution();
-	string str;
+	string str = "PAYPALISHIRING";
+	// string str = "A";
+	cout << solver.convert(str, 14) << endl;
 	
 	return 0;
 }
